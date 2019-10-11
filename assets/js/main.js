6e7015c9c1260e54c375a381ccaa9a71bc3d3922
ThoriumSnake <thoriumsnake@outlook.com>
2019-10-10 23:34:06 -0300

    some weird javascript shit, dont even ask me, also fixed the articles container stuff

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,38 +1,105 @@
-var articleButtons = document.getElementsByClassName("art-button");
-var elemArray = [document.getElementsByClassName("header-inner"), document.getElementsByClassName("header-icon"), document.getElementsByClassName("buttons"), document.getElementsByClassName("footer")];
+var articleButtons = document.getElementsByClassName('art-button');
+var initPageElements = [document.getElementsByClassName('header-inner'), document.getElementsByClassName('header-icon'), document.getElementsByClassName('buttons'), document.getElementsByClassName('footer')];
+var activeArticle;
+var articlesWrapper = document.getElementsByClassName("articles")[0];
 
-function HidePage(){
+function HideInitialPage(){
     //For fixing the position and opacity of some elements, because their animations are removed
-    elemArray[0][0].style.height = "30%";
-    elemArray[1][0].style.opacity = "1";
-    elemArray[1][0].style.top = "20%";
-    document.getElementsByClassName("buttons")[0].style.top = "80%";
+    initPageElements[0][0].style.height = '30%';
+    initPageElements[1][0].style.opacity = '1';
+    initPageElements[1][0].style.top = '20%';
+    document.getElementsByClassName('buttons')[0].style.top = '80%';
 
-    for (let i = 0; i < elemArray.length; i++) {
-        elemArray[i][0].classList.add("article-visible");          
+    for (let i = 0; i < initPageElements.length; i++) {
+        initPageElements[i][0].classList.add("article-is-visible");          
+    }
+
+    document.getElementsByClassName('header')[0].style.display = 'none'
+    document.getElementsByClassName('footer')[0].style.display = 'none'
+}
+
+function RevealArticle(){
+    articlesWrapper.style.display = 'flex'
+    articlesWrapper.classList.add('article-fade-in');
+
+    activeArticle.style.display = 'initial'
+}
+
+function AddButtonListeners(){
+    for (let item of articleButtons){
+        item.addEventListener('click', function(){
+            activeArticle = document.getElementsByClassName(item.classList[1])[1]
+            HideInitialPage();
+            RevealArticle();
+        });
     }
 }
 
-for (let item of articleButtons) {
-    item.addEventListener('click', function(){
-        HidePage();
-        document.getElementsByClassName("articles")[0].style.display = "initial";
-        document.getElementsByClassName("articles")[0].classList.add("article-delay"); 
-        document.getElementsByClassName(item.classList[1])[1].classList.add("article-delay");  
-        document.getElementsByClassName(item.classList[1])[1].style.display = "initial" 
-    })
-}
-/*document.getElementById("description-btn").addEventListener('click', function(){
-    document.getElementsByClassName("description")[0].style.display = "initial";
-    HidePage();
-})
+AddButtonListeners();
 
-document.getElementById("report-btn").addEventListener('click', function(){
-    document.getElementsByClassName("report")[0].style.display = "initial";
-    HidePage();
-})
 
-document.getElementById("containment-btn").addEventListener('click', function(){
-    document.getElementsByClassName("containment")[0].style.display = "initial";
-    HidePage();
-})*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function AddButtonListeners(){
+//     for (let item of articleButtons) {
+//         item.addEventListener('click', function(){
+//             activeArticle = document.getElementsByClassName(item.classList[1])[1]
+//             RemoveArtClasses();
+//             HideInitialPage();
+//             document.getElementsByClassName("articles")[0].style.display = "initial";
+//             document.getElementsByClassName("articles")[0].classList.add("article-fade-in"); 
+//             activeArticle.classList.add("article-fade-in");  
+//             activeArticle.style.display = "initial" 
+//         })
+//     }
+// }
+
+// function AddCloseButtonListener(){
+//         document.getElementsByClassName("close")[0].addEventListener('click', function(){
+//             articles.classList.add("article-fade-out");
+//             articles.classList.remove("article-fade-in");
+            
+//             activeArticle.classList.add("article-fade-out"); 
+//             activeArticle.classList.remove("article-fade-in");
+            
+//             setInterval(function(){
+//                 articles.style.display = "none";
+//             }, 325)
+            
+//             for (let i = 0; i < initPageElements.length; i++) {
+//                 initPageElements[i][0].classList.remove("article-is-visible");    
+//                 initPageElements[i][0].classList.add("article-not-visible");     
+//             }
+
+//             for (let i = 0; i < initPageElements.length; i++) {
+//                 setInterval(function(){
+//                     initPageElements[i][0].style.animation = "none";
+//                     initPageElements[i][0].classList.remove("article-not-visible");   
+//                 }, 20000)                      
+//             }
+//     })
+// }
+
+// function RemoveArtClasses(){
+//     if(articles.classList.contains("article-fade-out")){
+//         articles.classList.remove("article-fade-out");
+//         activeArticle.classList.remove("article-fade-out");}
+// }
+
+// AddButtonListeners();
+// AddCloseButtonListener();
